0fbd3ef16b3a35e8b3577a3df2e7bfe193d45caf
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-06-12 21:21:53 -0300

    MVC Basica

---
+++ MVC/MVCBasica/MVCBasica/Models/Enums/TipoFornecedor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasica.Models.Enums
+{
+    public enum TipoFornecedor
+    {
+        pessoaFisica = 1,
+        pessoaJuridica = 2
+    }
+}
---
+++ MVC/MVCBasica/MVCBasica/Models/Fornecedor.cs
@@ -0,0 +1,21 @@
+﻿using MVCBasica.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasica.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+
+    
+}
